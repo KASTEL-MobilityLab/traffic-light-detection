fccc209c0664e3f8f49d6d59e4c93338425e58c2
Nikolai Polley <nikolai.polley@kit.edu>
2024-05-14 16:32:57 +0200

    Training code of Yolov8x model on DTLD

--- train_yolo.py
+++ train_yolo.py
@@ -1,8 +1,39 @@
 from ultralytics import YOLO, RTDETR
 
+
 # Load a model
-#model = RTDETR('rtdetr-l.pt')  # build a new model from YAML
-#model = YOLO("yolov8x.yaml")
-model = YOLO("ultralytics_modified/ultralytics/cfg/models/v8/yolov8.yaml") #xl
-# Train the model
-results = model.train(data='custom_split.yaml', name='insert_name', epochs=100, imgsz=1920, device=0, batch=4, patience=0, mosaic=0.3, scale=0.7, save_period=1, cos_lr=True, fliplr=0, shear=0.1, copy_paste=0.1, perspective=0.0002,amp=False, mixup=0.1, degrees=5, cache="ram", workers=24,optimizer="AdamW", lr0=0.001, lrf=0.00005, translate=0, hsv_v=0.7, hsv_h=0.05)
+model = YOLO('yolov8x.yaml')
+
+# Train the model: Here training code for Yolov8x on DTLD dataset
+results = model.train(data='/workspace/traffic-light-detection/custom.yaml',
+                      epochs=100, # train for 100 epochs
+                      imgsz=2048, # full image size of DTLD
+                      workers=24, # adapt to available CPU cores
+                      device="0", # add more GPUs if available
+                      batch=6, # relative to how much GPU memory you have, this was used for H100 80GB
+                      nbs=64,
+                      patience=0, # disable early stopping
+                      project="yolo8x-traffic-light-detection",
+                      name="yolo8x-traffic-light-detection",
+                      pretrained=True, # use MS-Coco weights for initialization
+                      optimizer="AdamW",
+                      lr0=0.0001,
+                      warmup_epochs=1, # one epoch warmup
+                      warmup_bias_lr = 1e-9,
+                      warmup_momentum = 1e-9,
+                      cos_lr=True,
+                      lrf=0, # decay learning rate to zero with cosine function
+                      label_smoothing=0.001, # small regularization
+                      mosaic=0.6, # refer all following augmentations to here: https://docs.ultralytics.com/reference/data/augment/
+                      scale=0.4,
+                      degrees= 5,
+                      translate=0.1,
+                      save_period=1,
+                      fliplr=0.5,
+                      mixup=0.2,
+                      shear=0.3,
+                      copy_paste=0.4,
+                      close_mosaic=5,
+                      cls=0.7,
+                      plots=True
+                      )
